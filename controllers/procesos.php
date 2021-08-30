353e82a65175e6694c7cf086a9d2973665f5300d
marimartinez21 <87771328+marimartinez21@users.noreply.github.com>
2021-08-29 20:47:16 -0600

    update

--- controllers/procesos.php
+++ controllers/procesos.php
@@ -1,53 +1,51 @@
-<?php 
-   function Accesologia($user, $passw) 
-   {
-       $consultas = new login();
-       $data = $consultas->getDataUser($user);
+<?php
+    function AccesoLogin($user, $passw)
+    {
+        $consultas = new Login();
+        $data = $consultas->getDataUser($user);
 
-       if($data)
+        if($data)
         {
             foreach ($data AS $result)
-           {
-            $idusuario = $result['idusuario'];
-            $hash = $result['clave'];
-            $tipo = $result['tipo'];
-            $estado = $result['estado'];
-
-            
-           }
-
-           if($estado  == 1)
-           {
-               if(password_verify($pass,$hash))
-                { 
-
-                  if($tipo == 1) //vista Admin
-                 { 
-                     $_SESSION["idusuario"] = $idusuario;
-                     $_SESSION["user"] = $user;
-                     $_SESSION["tipo"] = $tipo;
-                     header("location: ../views/Admin/");
-                 }
-                   else  
-                   {
-                    $_SESSION["idusuario"] = $idusuario;
-                    $_SESSION["user"] = $user;
-                    $_SESSION["tipo"] = $tipo;
-                    header("location: ../views/operador/"); 
-                   }  
-
-
+            {
+                $idusuario = $result['idusuario'];
+                $hash = $result['clave'];
+                $tipo = $result['tipo'];
+                $estado = $result['estado'];
+            }
+            if($estado == 1)
+            {
+                if(password_verify($passw,$hash))
+                {
+                    if($tipo == 1) //Vista Admin
+                    {
+                        $_SESSION["idusuario"] = $idusuario;
+                        $_SESSION["user"] = $user;
+                        $_SESSION["tipo"] = $tipo;
+                        header("Location: ../views/admin/");
+                    }
+                    else
+                    {
+                        $_SESSION["idusuario"] = $idusuario;
+                        $_SESSION["user"] = $user;
+                        $_SESSION["tipo"] = $tipo;
+                        header("Location: ../views/operador/");
+                    }
+                }
+                else
+                {
+                    header("Location:../index.php?msj=".base64_decode("Contraseña incorrecta..."));
+                }
+            }
+            else
+            {
+                header("Location:../index.php?msj=".base64_decode("El usuario no tiene permisos de acceso..."));
+            }
         }
-        else
+       else
         {
-            header("location:../index.php?msj=".base64_encode("el usuario no tiene permisos de  acceso..."));
-
+            header("Location:../index.php?msj=".base64_decode("El usuario no existe..."));
         }
-
-        else
-         {
-            header("location:../index.php?msj=".base64_encode("el usuario no  exite..."));
- 
-         }
     }
+
 ?>
