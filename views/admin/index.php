3994ba81c93388bb351596a05d99101e1a557e3a
marimartinez21 <87771328+marimartinez21@users.noreply.github.com>
2021-09-01 10:09:40 -0600

    update

--- views/admin/index.php
+++ views/admin/index.php
@@ -12,26 +12,8 @@ $redic->Admin();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administrador</title>
-    <!--CSS-->
-    <link rel="stylesheet" href="../../public/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../../public/css/boststrap-theme.css">
-    <link rel="stylesheet" href="../../public/css/estilo.css">
-    <link rel="stylesheet" href="../../public/css/alertify.min.css" />
-    <link rel="stylesheet" href="../../public/css/default.min.css" />
-    <!--JS-->
-    <script src="../../public/js/alertify.min.js"></script>
-    <script src="../../public/js/jquery-3.5.1.slim.min.js"></script>
-    <script src="../../public/js/jquery-1.9.1.min.js"></script>
-    <script src="../../public/js/bootstrap.min.js"></script>
-    <script src="../../public/js/funciones-navbar.js"></script>
-    <script src="https://kit.fontawesome.com/05f4903dc9.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <?php include 'navbar/navbar.php';?>
-    <div class="container-fluid" id="contenido">
-        <div class="alert alert-success" style="width:400px;">
-           <h6><b>Bienvenido/a: <?php echo $user; ?></b></h6>
-        </div>
-    </div>  
+    <h3><b>Bienvenido/a <?php echo $user; ?></b></h3>
 </body>
 </html>
