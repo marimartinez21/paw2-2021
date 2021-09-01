7f3933966455899777fb3f418ec2169e9f504548
marimartinez21 <87771328+marimartinez21@users.noreply.github.com>
2021-09-01 10:36:34 -0600

    update

--- models/procesos.php
+++ models/procesos.php
@@ -12,19 +12,48 @@
             $tipo = $result['tipo'];
             $estado = $result['estado'];
         }
+
         if($estado) == 1)
         {
            if(password_verify($passw,$hash))
            {
                if(tipo == 1) //Vista Admin
                {
+                
+                
+                    $_SESSION ['idusuario']=$idusuario;
+                    $_SESSION ['user']= $user;
+                    $_SESSION ['tipo']= $tipo;
+                    header("location: ../views/operador/");
+        }
 
+        else 
+           {
+            $_SESSION ['idusuario']=$idusuario;
+            $_SESSION ['user']= $user;
+            $_SESSION ['tipo']=$tipo;
+            header("location: ../views/operador/");
+    
+           }
+         }
         
         else
-        {
-    {
+           { 
+            header("Location:../index.php?msj=".base64_encode("contraseña incorrecta..."));
+           }
+         
+         }
+           else
+           { 
             header("Location:../index.php?msj=".base64_encode("El usuario no tiene permisos de acceso..."));
-    }
+           }
+        }
 
-    }
+        else
+        { 
+         header("Location:../index.php?msj=".base64_encode("El usuario no existe..."));
+        }
+
+     }
+
 ?>
